888888c0bfff144c7857ce57754fc8f792db0a9b
Carlosho23 <c.oliveira@nextage.com.br>
2025-07-16 23:52:24 -0300

    feat: Implementa consumo de mensagem do RabbitMQ

--- src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,9 +1,16 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.dtos.NotificationRecordCommandDto;
+import com.ead.notification.enums.NotificationStatus;
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationModelRepository;
 import com.ead.notification.services.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +18,12 @@ public class NotificationServiceImpl implements NotificationService {
 
     final NotificationModelRepository notificationModelRepository;
 
+    @Override
+    public NotificationModel saveNotification(NotificationRecordCommandDto notificationRecordCommandDto) {
+        NotificationModel notificationModel = new NotificationModel();
+        BeanUtils.copyProperties(notificationRecordCommandDto, notificationModel);
+        notificationModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        notificationModel.setNotificationStatus(NotificationStatus.CREATED);
+        return notificationModelRepository.save(notificationModel);
+    }
 }
